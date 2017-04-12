887c7d09b5a7fdad649dc7644807fc9f0345d564
twinls1247 <twinls1247@yahoo.com>
2017-04-12 09:25:16 -0400

    Add files via upload
    This is a Electronic Health Information Law Data Map application.  Parsing of all the data into different category modules of page is not yet completed.

---
+++ GetEHIText.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GetEHIText.aspx.cs" Inherits="JsonDemo.GetEHIText" %>
+
---
+++ GetEHIText.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Script.Serialization;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using EHI;
+
+namespace JsonDemo
+{
+    public partial class GetEHIText : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string jsonString =js.Serialize(GetEHITextByChild(Request["EHIflare"]));
+            Response.Write(jsonString);    
+        }
+
+        private object GetEHITextByChild(string name)
+        {
+            EHI.RootObject rObject = new EHI.RootObject();
+            rObject.name = name;
+
+            return rObject;
+        }   
+    }
+}
